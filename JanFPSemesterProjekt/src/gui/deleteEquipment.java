59f2f432a477dcd564d69d21b8853e5decd9a3f3
thatguy12322 <103989999+thatguy12322@users.noreply.github.com>
2022-12-15 19:57:40 +0100

    Update deleteEquipment.java

--- JanFPSemesterProjekt/src/gui/deleteEquipment.java
+++ JanFPSemesterProjekt/src/gui/deleteEquipment.java
@@ -7,10 +7,16 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
 
 public class deleteEquipment extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField_SerialNumber;
 
 	/**
 	 * Launch the application.
@@ -31,21 +37,39 @@ public class deleteEquipment extends JDialog {
 	public deleteEquipment() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblSerialNumber = new JLabel("Intast varens  serie nummer:");
+			lblSerialNumber.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			lblSerialNumber.setBounds(42, 109, 163, 14);
+			contentPanel.add(lblSerialNumber);
+		}
+		{
+			textField_SerialNumber = new JTextField();
+			textField_SerialNumber.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			textField_SerialNumber.setBounds(215, 106, 86, 20);
+			contentPanel.add(textField_SerialNumber);
+			textField_SerialNumber.setColumns(10);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton BtnDelete = new JButton("Fjern");
+				BtnDelete.setActionCommand("OK");
+				buttonPane.add(BtnDelete);
+				getRootPane().setDefaultButton(BtnDelete);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
